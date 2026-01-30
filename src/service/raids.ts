d9f11e3ba49bd91a09da62dc9380167e59dd37e3
caxerx <caxerx@gmail.com>
2026-01-30 18:22:08 +0900

    feat: add all raid type and added alias

--- src/service/raids.ts
+++ src/service/raids.ts
@@ -1,14 +1,64 @@
 import { RaidType } from "@/generated/prisma/enums";
 
 export const raids = [
-    {
-        id: RaidType.Kirollas,
-        simpleName: 'raids:kirollas.simpleName',
-        originalName: 'raids:kirollas.originalName',
-    },
-    {
-        id: RaidType.Carno,
-        simpleName: 'raids:carno.simpleName',
-        originalName: 'raids:carno.originalName',
-    },
-]
+  {
+    id: RaidType.Kirollas,
+    simpleName: "raids:kirollas.simpleName",
+    originalName: "raids:kirollas.originalName",
+    shortSymbol: "-0",
+  },
+  {
+    id: RaidType.Carno,
+    simpleName: "raids:carno.simpleName",
+    originalName: "raids:carno.originalName",
+    shortSymbol: "-B",
+  },
+  {
+    id: RaidType.Zenas,
+    simpleName: "raids:zenas.simpleName",
+    originalName: "raids:zenas.originalName",
+    shortSymbol: "ZN",
+  },
+  {
+    id: RaidType.Erenia,
+    simpleName: "raids:erienia.simpleName",
+    originalName: "raids:erienia.originalName",
+    shortSymbol: "ER",
+  },
+  {
+    id: RaidType.Bellia,
+    simpleName: "raids:bellia.simpleName",
+    originalName: "raids:bellia.originalName",
+    shortSymbol: "BL",
+  },
+  {
+    id: RaidType.Paimon,
+    simpleName: "raids:paimon.simpleName",
+    originalName: "raids:paimon.originalName",
+    shortSymbol: "PI",
+  },
+  {
+    id: RaidType.RevenantPaimon,
+    simpleName: "raids:revenantpaimon.simpleName",
+    originalName: "raids:revenantpaimon.originalName",
+    shortSymbol: "P2",
+  },
+  {
+    id: RaidType.Alzanor,
+    simpleName: "raids:alzanor.simpleName",
+    originalName: "raids:alzanor.originalName",
+    shortSymbol: "AZ",
+  },
+  {
+    id: RaidType.Valehir,
+    simpleName: "raids:valehir.simpleName",
+    originalName: "raids:valehir.originalName",
+    shortSymbol: "VH",
+  },
+  {
+    id: RaidType.Asgobas,
+    simpleName: "raids:asgobas.simpleName",
+    originalName: "raids:asgobas.originalName",
+    shortSymbol: "AS",
+  },
+];
