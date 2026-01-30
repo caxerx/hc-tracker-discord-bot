93d8d185512a10e9bc01d23075f6027dc44fc2fd
caxerx <caxerx@gmail.com>
2026-01-31 04:29:55 +0900

    fix: workflow plugin

--- commandkit.config.ts
+++ commandkit.config.ts
@@ -1,25 +1,24 @@
-import 'dotenv/config';
-import { defineConfig, noBuildOnly } from 'commandkit/config';
-import { cache } from '@commandkit/cache';
-import { i18n } from '@commandkit/i18n';
-import { setDriver, tasks } from '@commandkit/tasks';
-import { devtools } from '@commandkit/devtools';
+import "dotenv/config";
+import { defineConfig, noBuildOnly } from "commandkit/config";
+import { cache } from "@commandkit/cache";
+import { i18n } from "@commandkit/i18n";
+import { setDriver, tasks } from "@commandkit/tasks";
+import { devtools } from "@commandkit/devtools";
 // @ts-ignore
-import { BullMQDriver } from '@commandkit/tasks/bullmq';
-import { workflow } from '@commandkit/workflow';
-
+import { BullMQDriver } from "@commandkit/tasks/bullmq";
+import { workflow } from "@commandkit/workflow";
 
 // set up the driver in non-build processes only
 const setupDriver = noBuildOnly(() => {
-    setDriver(
-        new BullMQDriver({
-            url: process.env.REDIS_URL,
-        }),
-    );
+  setDriver(
+    new BullMQDriver({
+      url: process.env.REDIS_URL,
+    }),
+  );
 });
 
 setupDriver();
 
 export default defineConfig({
-    plugins: [cache(), i18n(), tasks(), devtools(), workflow()],
+  plugins: [cache(), i18n(), tasks(), devtools(), workflow()],
 });
